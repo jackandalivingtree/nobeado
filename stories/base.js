1b014c9a1310c22e5ecb27050ea51b5a351c1d23
nattyan-tv <undirect_sbv@yahoo.co.jp>
2021-08-18 20:37:44 +0900

    first commit

---
+++ stories/base.js
@@ -0,0 +1,52 @@
+function getCookieArray(){
+    var arr = new Array();
+    if(document.cookie != ''){
+        var tmp = document.cookie.split('; ');
+        for(var i=0;i<tmp.length;i++){
+            var data = tmp[i].split('=');
+            arr[data[0]] = decodeURIComponent(data[1]);
+        }
+    }
+    return arr;
+}
+
+
+var arr = getCookieArray();
+var cookie_game = arr['start_game'];
+var cookie_name = arr['name'];
+
+
+if (cookie_game != 1){
+    alert('内部エラーが発生しました。\nホームに戻ります。\n\nユーザーデータが読み込めませんでした。')
+    open( "/jlt/home.html", "_top" ) ;
+}
+
+
+function menu_open(){
+    menu_img.innerHTML = '<a href="javascript:menu_close()"><img src="back_menu.png" class="back_menu_image"></a><a href="javascript:menu_top()"><img src="top_menu.png" class="top_menu_image"></a><a href="javascript:menu_skip()"><img src="skip_menu.png" class="skip_menu_image"></a>'
+}
+
+function menu_close(){
+    menu_img.innerHTML = '<a href="javascript:menu_open()"><img src="menu.png" class="menu_image"></a>'
+}
+
+function menu_top(){
+    location.href = ('/jlt/story_select.html')
+}
+
+function menu_skip(){
+    location.href = ('/jlt/story_select.html')
+}
+
+function set_texts(type){
+    if(type == 0){
+        text_box.innerHTML = '<a href="javascript:exit()"><img src="text_box.png"><p id="chara_name" class="name">' + cname + '</p><p id="message" class="mes">' + cmes + '</p></a>'
+        character_image.innerHTML = '<img src="' + cimg + '" class="chara_image">'
+    }else if(type == 1){
+        text_box.innerHTML = ''
+        character_image.innerHTML = ''
+    }else{
+        text_box.innerHTML = ' <a href="javascript:next' + story_page + '()"><img src="text_box.png"><p id="chara_name" class="name">' + cname + '</p><p id="message" class="mes">' + cmes + '</p></a>'
+        character_image.innerHTML = '<img src="' + cimg + '" class="chara_image">'
+    }
+}
